a2cccdb7a67daecb10c836055f8bec235ceac91d
Hannah Nefdt <hannahn@flowsa.com>
2022-04-23 17:37:38 +0200

    draggable handle

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,26 @@
 import React, { useState } from 'react';
-import { arrayMove, SortableContainer, SortableElement, } from 'react-sortable-hoc';
+import { arrayMove, SortableContainer, SortableElement, SortableHandle } from 'react-sortable-hoc';
 import { ITEMS } from './data';
-
+// import 'bootstrap-icons'
+import { CircleFill, GripHorizontal } from 'react-bootstrap-icons';
 import './App.css';
 
 // Reusable array headings for mobile purposes
-const headings = ['Worker ID', 'Worker Name', 'Overtime', 'Manual Hours', 'Hours', 'Total Hours', 'Status']
+const headings = ['Worker ID', 'Worker Name', 'Overtime', 'Manual Hours', 'Hours', 'Total Hours']
+
+const DragHandle = SortableHandle(() => (
+    <GripHorizontal size={24} />
+  ));
 
 const SortableItem = SortableElement(({ value }) => (
     <tr>
         <td scope="row" data-label={headings[0]} className="td-id">{value.id}</td>
-        <td data-label={headings[1]} className="td-worker-name">{value.workerName}</td>
+        <td data-label={headings[1]} className="td-worker-name"><CircleFill color={`${value.status ?? 'red'}`} size={10} />&nbsp;&nbsp;{value.workerName}</td>
         <td data-label={headings[2]}>{value.extraHours}</td>
         <td data-label={headings[3]}>{value.manualHours}</td>
         <td data-label={headings[4]}>{value.hours}</td>
         <td data-label={headings[5]}>{value.totalHours}</td>
-        <td data-label={headings[6]} class={`text-${value.status ?? 'red'}`}>[icon]</td>
+        <td className="drag"><DragHandle /></td>
     </tr>
 ));
 
@@ -26,6 +31,7 @@ const SortableList = SortableContainer(({ items }) => {
                 {headings.map((heading, i) => (
                     <th key={`th-${i}`} scope="col">{heading}</th>
                 ))}
+                <td className="drag">&nbsp;</td>
             </thead>
             <tbody>
                 {items.map((value, index) => (
@@ -50,7 +56,7 @@ function SortableComponent() {
 
     return (
         <div className="table-container">
-            <SortableList items={data} onSortEnd={onSortEnd} />
+            <SortableList items={data} onSortEnd={onSortEnd} useDragHandle={true} />
         </div>
     );
 }
